67f261361c8a7f6bbb15c847a79e5f29f8f275d9
Dylan Knauer <dylan@knauers.ca>
2023-11-19 18:17:58 -0500

    N/A - work

--- watch-data-collector/ContentView.swift
+++ watch-data-collector/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+struct WatchMotionRecording : Identifiable{
+    let id = UUID()
+    let description: String
+    let dateTime: Date
+    let duration: TimeInterval
+}
+
 struct ContentView: View {
+   @State var recordings = [
+        WatchMotionRecording(description: "one", dateTime: Date(), duration: 1000),
+        WatchMotionRecording(description: "two", dateTime: Date(), duration: 1000),
+        WatchMotionRecording(description: "three", dateTime: Date(), duration: 1000),
+        WatchMotionRecording(description: "four", dateTime: Date(), duration: 1000),
+        WatchMotionRecording(description: "five", dateTime: Date(), duration: 1000),
+        WatchMotionRecording(description: "six", dateTime: Date(), duration: 1000)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            
+                List(recordings) { recording in
+                    Text(recording.description)
+                }
+                .navigationTitle("Watch Data Collector")
+                .toolbar {
+                    Button(action: {
+                        let recording = WatchMotionRecording(description: "watch", dateTime: Date(), duration: 1000)
+                        recordings.append(recording)
+                    }) {
+                        Image(systemName: "plus.circle")
+                    }
+                }
         }
-        .padding()
+        
     }
 }
 
